9cdbc2b1cbe3d84605d99a22705b0ec3ccd6b0d0
max2556 <46759632+max2556@users.noreply.github.com>
2021-11-03 18:48:41 +0300

    Парочка изменений

--- config.js
+++ config.js
@@ -25,42 +25,5 @@ let defaultSettings = {
     ]
 };
 
-
-
-
-
-//-----
-//ВСЁ, ЧТО НИЖЕ - ЛУЧШЕ НЕ МЕНЯТЬ(ИЛИ ПРОКОНСУЛЬТИРОВАТЬСЯ СО СПЕЦИАЛИСТОМ)
-//-----
-
-//Переменные используемые в коде!!! Не менять их имени ни в коем разе, иначе - КРИНДЕЦ!!!!
-let cellSize, traceBlockColor, backgroundColor, emptyColor, deltaLight, border, gradientBlocks, colors;
-checkForSettingsInLocalStorage();
-
-
-//Процедура, которая задает переменным из 2-й строчки значения
-//Не менять!!
-function checkForSettingsInLocalStorage() {
-    let storage = localStorage.getItem("settings");
-    if (storage === null) {
-        localStorage.setItem("settings", JSON.stringify(defaultSettings));
-        storage = defaultSettings;
-    } else { storage = JSON.parse(storage); }
-    cellSize = storage.cellSize;
-    traceBlockColor = storage.traceBlockColor;
-    backgroundColor = storage.backgroundColor;
-    emptyColor = storage.emptyColor;
-    deltaLight = storage.deltaLight;
-    border = storage.border;
-    gradientBlocks = storage.gradientBlocks;
-    colors = storage.colors;
-
-}
-
-//Метод меняет значение какой-нибудь штуки в настройках
-//Можно использовать для какой-нибудь менюшки настроек, чтобы пользователь мог менять параметры на свой вкус
-function changeSettingsItem(itemName, itemValue) {
-    let storage = JSON.parse(localStorage.getItem("settings"));
-    storage[itemName] = itemValue;
-    localStorage.setItem("settings", JSON.stringify(storage));
-}
+//Для загрузки стандартных настроек впишите в консоли браузера cleanTheStorage();
+//И перезагрузите страницу
---
+++ storageScipt.js
@@ -0,0 +1,42 @@
+//-----
+//ВСЁ, ЧТО НИЖЕ - ЛУЧШЕ НЕ МЕНЯТЬ(ИЛИ ПРОКОНСУЛЬТИРОВАТЬСЯ СО СПЕЦИАЛИСТОМ)
+//-----
+let storageKey = "settings";
+
+
+
+//Переменные используемые в коде!!! Не менять их имени ни в коем разе, иначе - КРИНДЕЦ!!!!
+let cellSize, traceBlockColor, backgroundColor, emptyColor, deltaLight, border, gradientBlocks, colors;
+checkForSettingsInLocalStorage();
+
+
+//Процедура, которая задает переменным из 2-й строчки значения
+//Не менять!!
+function checkForSettingsInLocalStorage() {
+    let storage = localStorage.getItem(storageKey);
+    if (storage === null) {
+        localStorage.setItem(storageKey, JSON.stringify(defaultSettings));
+        storage = defaultSettings;
+    } else { storage = JSON.parse(storage); }
+    cellSize = storage.cellSize;
+    traceBlockColor = storage.traceBlockColor;
+    backgroundColor = storage.backgroundColor;
+    emptyColor = storage.emptyColor;
+    deltaLight = storage.deltaLight;
+    border = storage.border;
+    gradientBlocks = storage.gradientBlocks;
+    colors = storage.colors;
+
+}
+
+//Метод меняет значение какой-нибудь штуки в настройках
+//Можно использовать для какой-нибудь менюшки настроек, чтобы пользователь мог менять параметры на свой вкус
+function changeSettingsItem(itemName, itemValue) {
+    let storage = JSON.parse(localStorage.getItem(storageKey));
+    storage[itemName] = itemValue;
+    localStorage.setItem(storageKey, JSON.stringify(storage));
+}
+
+function clearTheStorage() {
+    localStorage.removeItem(storageKey);
+}
